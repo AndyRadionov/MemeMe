0a1c7e993fbf2a61bdcf2b4c514f4f1c53f9c2a9
Andrey Radionov <andyomsk@gmail.com>
2020-05-05 19:44:56 +0600

    Design updated

---
+++ MemeMe-1.0/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe-1.0
+//
+//  Created by Andy on 05.05.2020.
+//  Copyright © 2020 AndyRadionov. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
--- MemeMe-1.0/MemeViewController.swift
+++ MemeMe-1.0/MemeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,
+class MemeViewController: UIViewController,
     UIImagePickerControllerDelegate,
     UINavigationControllerDelegate,
     UITextFieldDelegate {
@@ -20,14 +20,14 @@ class ViewController: UIViewController,
     
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth:  4
+        NSAttributedString.Key.strokeWidth:  -3
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagePickerView.contentMode = .scaleToFill
+        imagePickerView.contentMode = .scaleAspectFit
         topTextField.text = "TOP"
         bottomTextField.text = "BOTTOM"
         topTextField.textAlignment = .center
@@ -93,12 +93,27 @@ class ViewController: UIViewController,
         return true
     }
     
-    func subscribeToKeyboardNotifications() {
+    private func generateMemedImage() -> UIImage {
+
+        // TODO: Hide toolbar and navbar
+
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        // TODO: Show toolbar and navbar
+
+        return memedImage
+    }
+    
+    private func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    func unsubscribeFromKeyboardNotifications() {
+    private func unsubscribeFromKeyboardNotifications() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
