433b80ce3380beda76cda7710380ed6d7184341c
Andrey Radionov <andyomsk@gmail.com>
2020-05-05 11:54:13 +0600

    Initial Commit

---
+++ MemeMe-1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe-1.0
+//
+//  Created by Andy on 05.05.2020.
+//  Copyright © 2020 AndyRadionov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
